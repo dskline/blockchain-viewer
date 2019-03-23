4b91f8400ed1edadc181b253bf7f7029d74dad2b
dskline <dskline1@gmail.com>
2019-03-23 19:10:21 -0400

    Updates

---
+++ precache-manifest.1d1adff0019fd4455cf47d89ac9c354c.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "31bd6c236432c57eeb3e",
+    "url": "/blockchain-viewer/static/js/main.b9694b18.chunk.js"
+  },
+  {
+    "revision": "8030cda6a7bfaf75094c",
+    "url": "/blockchain-viewer/static/js/runtime~main.464b2d1e.js"
+  },
+  {
+    "revision": "511d3fa6c818f3e96ec3",
+    "url": "/blockchain-viewer/static/js/2.e0bebc98.chunk.js"
+  },
+  {
+    "revision": "5c56b509362f9e55e164e093f6789ec4",
+    "url": "/blockchain-viewer/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.0b8668f4459f6f1d3518cf9324c7b311.js"
+  "/blockchain-viewer/precache-manifest.1d1adff0019fd4455cf47d89ac9c354c.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/blockchain-viewer/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
